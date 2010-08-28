60120762ebc7712060192c2ecd1f305c1a21feec
Rehno Lindeque <errantkid@gmail.com>
2010-08-28 15:25:50 +0200

    Replaced the skybox with a json version Started adding a div to the index for logging messages

--- main.js
+++ main.js
@@ -178,36 +178,78 @@
     y: 1.22,
     z: 1.22
   }, platformGeometry("level2"), levelNodes[2].archerTowers, levelNodes[2].catapultTowers)));
-  skyboxNode = SceneJS.scale({
-    x: 100.0,
-    y: 100.0,
-    z: 100.0
-  }, SceneJS.material({
-    baseColor: {
-      r: 1.0,
-      g: 1.0,
-      b: 1.0
+  /*
+  skyboxNode =
+    SceneJS.scale(
+      { x: 100.0, y: 100.0, z: 100.0 },
+      SceneJS.material(
+        baseColor:      { r: 1.0, g: 1.0, b: 1.0 }
+        specularColor:  { r: 1.0, g: 1.0, b: 1.0 }
+        specular:       0.0
+        shine:          0.0
+        SceneJS.texture(
+          { layers: [{uri:"textures/sky.png"}] }
+          SceneJS.geometry(
+            type:       "Skybox"
+            primitive:  "triangles"
+            positions:  [1, 1, 1,-1, 1, 1,-1,-1, 1, 1,-1, 1, 1, 1, 1, 1,-1, 1, 1,-1,-1, 1, 1,-1, 1, 1, 1, 1, 1,-1,-1, 1,-1,-1, 1, 1,-1, 1, 1,-1, 1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1, 1,-1,-1, 1,-1, 1,-1,-1, 1, 1,-1,-1,-1,-1,-1,-1, 1,-1, 1, 1,-1]
+            uv:         [1,1,0,1,0,0,1,0,0,1,0,0,1,0,1,1,1,0,1,1,0,1,0,0,1,1,0,1,0,0,1,0,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1]
+            indices:    [0,1,2,0,2,3,4,5,6,4,6,7,8,9,10,8,10,11,12,13,14,12,14,15,16,17,18,16,18,19,20,21,22,20,22,23]
+          ) # geometry
+        ) # texture
+      ) # material
+    ) # scale
+  */
+  skyboxNode = SceneJS.createNode({
+    type: "scale",
+    cfg: {
+      x: 100.0,
+      y: 100.0,
+      z: 100.0
     },
-    specularColor: {
-      r: 1.0,
-      g: 1.0,
-      b: 1.0
-    },
-    specular: 0.0,
-    shine: 0.0
-  }, SceneJS.texture({
-    layers: [
+    nodes: [
       {
-        uri: "textures/sky.png"
+        type: "material",
+        cfg: {
+          baseColor: {
+            r: 1.0,
+            g: 1.0,
+            b: 1.0
+          },
+          specularColor: {
+            r: 1.0,
+            g: 1.0,
+            b: 1.0
+          },
+          specular: 0.0,
+          shine: 0.0
+        },
+        nodes: [
+          {
+            type: "texture",
+            cfg: {
+              layers: [
+                {
+                  uri: "textures/sky.png"
+                }
+              ]
+            },
+            nodes: [
+              {
+                type: "geometry",
+                cfg: {
+                  primitive: "triangles",
+                  positions: [1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1],
+                  uv: [1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1],
+                  indices: [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23]
+                }
+              }
+            ]
+          }
+        ]
       }
     ]
-  }, SceneJS.geometry({
-    type: "Skybox",
-    primitive: "triangles",
-    positions: [1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1],
-    uv: [1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1],
-    indices: [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23]
-  }))));
+  });
   sceneLookAtURI = "SceneLookAt";
   sceneLookAtConfig = {
     uri: sceneLookAtURI,
@@ -312,7 +354,8 @@
     shine: 0.0
   }, numberedDaisNode(0), numberedDaisNode(1)));
   gameScene = SceneJS.scene({
-    canvasId: "gameCanvas"
+    canvasId: "gameCanvas",
+    loggingElementId: "scenejsLog"
   }, SceneJS.symbol({
     sid: "ArcherTower"
   }, BlenderExport.ArcherTower()), SceneJS.symbol({
