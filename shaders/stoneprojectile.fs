e9b27be12dcb524db699f53b8aab01ed8b14ac39
rehno-lindeque <errantkid@gmail.com>
2011-01-09 19:54:50 +0200

    Changed the projectile color to something more visible

--- shaders/stoneprojectile.fs
+++ shaders/stoneprojectile.fs
@@ -8,6 +8,6 @@ void main(void) {
   float dist = distance(gl_PointCoord, vec2(0.5, 0.5));
   if(dist > 0.5)
     discard;
-  gl_FragColor = vec4(0.1, 0.1, 0.1, 1.0);
+  gl_FragColor = vec4(0.4, 0.28, 0.15, 1.0);
 }
 
