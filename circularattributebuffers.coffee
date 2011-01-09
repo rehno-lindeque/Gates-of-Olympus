d909d552fe73c9c7e956639d2fea573410d0e007
rehno-lindeque <errantkid@gmail.com>
2011-01-09 11:16:35 +0200

    Making the circularattributebuffer more generic (this breaks at the moment though... busy fixing it)

--- circularattributebuffers.coffee
+++ circularattributebuffers.coffee
@@ -12,7 +12,6 @@ class CircularAttributeBuffers
     @t = 0.0
     @bottomOffset = 0
     @topOffset = 0
-    #@vertices = new Array(@size)
     @attributeBuffers = []
     @attributeQueues = [[]]
     @attributeInfos = []
@@ -46,16 +45,28 @@ class CircularAttributeBuffers
     )
     gl.bindBuffer(gl.ARRAY_BUFFER, @attributeBuffers[2])
     gl.bufferData(gl.ARRAY_BUFFER, @size * @attributeInfos[2].elements, gl.STREAM_DRAW)
+    null
   
   # Push one element onto the buffer (the effective time will be whatever t is when we call update(t)
   # The elements in total as well as each element itself must be boxed into arrays
   push: (elements) ->
-    for k in [0 .. @attributeBuffers.length - 1]
+    # Pre-condition: @elements.length == @attributeQueues.length - 1 (since it should not include "t")
+    for k in [0 .. @elements.length - 1]
       @attributeQueues[k].concat(elements[k])
+    null
+
+  # Get the range of the attribute buffer
+  getRange: -> [@bottomOffset, @topOffset]
   
   # Update all the attribute buffers with the elements pushed onto the queue and discard expired elements
   update: (gl, t) ->
     @t = t
+    
+    # Add t to the attributes
+    for k in [0..@attributeQueues[0].length-1]
+      @attributeQueues[@attributeQueues[0].length-1].push(t)
+    
+    # Push all the queues into the buffer objects
     for queue in @attributeQueues
       if (@topOffset < @bottomOffset && @topOffset + queue.length < @bottomOffset) || (@topOffset >= @bottomOffset && @topOffset + queue.length < @size)
         gl.bindBuffer(gl.ARRAY_BUFFER, @attributeBuffers[0])
@@ -66,13 +77,17 @@ class CircularAttributeBuffers
         else
           num = @size - @topOffset
         #todo: more to do here....!!!
+    
+    # Clear the queues
     @attributeQueues = [[]]
+    null
   
   bind: (gl, shaderLocations) ->
     for k in [0 .. @attributeBuffers.length - 1]
       gl.bindBuffer(gl.ARRAY_BUFFER, @attributeBuffers[k])
       gl.enableVertexAttribArray(shaderLocations[k])
       gl.vertexAttribPointer(shaderLocations[k], @attributeInfos[k].elements, @attributeInfos[k].glType, false, 0, 0)
+    null
   
   destroy: ->
     for buffer in @attributeBuffers
@@ -80,4 +95,4 @@ class CircularAttributeBuffers
     @attributeBuffers = []
     @attributeQueues = [[]]
     @attributeInfos = []
-
+    null
--- js/circularattributebuffers.js
+++ js/circularattributebuffers.js
@@ -37,46 +37,36 @@ CircularAttributeBuffers.prototype.create = function(gl) {
     glType: gl.FLOAT
   });
   gl.bindBuffer(gl.ARRAY_BUFFER, this.attributeBuffers[2]);
-  return gl.bufferData(gl.ARRAY_BUFFER, this.size * this.attributeInfos[2].elements, gl.STREAM_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, this.size * this.attributeInfos[2].elements, gl.STREAM_DRAW);
+  return null;
 };
 CircularAttributeBuffers.prototype.push = function(elements) {
-  var _result, k;
-  _result = [];
-  for (k = 0; (0 <= this.attributeBuffers.length - 1 ? k <= this.attributeBuffers.length - 1 : k >= this.attributeBuffers.length - 1); (0 <= this.attributeBuffers.length - 1 ? k += 1 : k -= 1)) {
-    _result.push(this.attributeQueues[k].concat(elements[k]));
+  var k;
+  for (k = 0; (0 <= this.elements.length - 1 ? k <= this.elements.length - 1 : k >= this.elements.length - 1); (0 <= this.elements.length - 1 ? k += 1 : k -= 1)) {
+    this.attributeQueues[k].concat(elements[k]);
   }
-  return _result;
+  return null;
+};
+CircularAttributeBuffers.prototype.getRange = function() {
+  return [this.bottomOffset, this.topOffset];
 };
 CircularAttributeBuffers.prototype.update = function(gl, t) {
-  var _i, _len, _ref, num, queue;
+  var k;
   this.t = t;
-  _ref = this.attributeQueues;
-  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-    queue = _ref[_i];
-    if ((this.topOffset < this.bottomOffset && this.topOffset + queue.length < this.bottomOffset) || ((this.topOffset >= this.bottomOffset) && this.topOffset + queue.length < this.size)) {
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.attributeBuffers[0]);
-      gl.bufferSubData(gl.ARRAY_BUFFER, this.topOffset * this.attributeInfos[0].elements, new Float32Array(queue));
-    } else {
-      if (this.topOffset < this.bottomOffset) {
-        num = this.bottomOffset - this.topOffset;
-      } else {
-        num = this.size - this.topOffset;
-      }
-    }
+  for (k = 0; (0 <= this.attributeQueues[0].length - 1 ? k <= this.attributeQueues[0].length - 1 : k >= this.attributeQueues[0].length - 1); (0 <= this.attributeQueues[0].length - 1 ? k += 1 : k -= 1)) {
+    this.attributeQueues[this.attributeQueues[0].length - 1].push(t);
   }
-  return (this.attributeQueues = [[]]);
+  this.attributeQueues = [[]];
+  return null;
 };
 CircularAttributeBuffers.prototype.bind = function(gl, shaderLocations) {
-  var _result, k;
-  _result = [];
+  var k;
   for (k = 0; (0 <= this.attributeBuffers.length - 1 ? k <= this.attributeBuffers.length - 1 : k >= this.attributeBuffers.length - 1); (0 <= this.attributeBuffers.length - 1 ? k += 1 : k -= 1)) {
-    _result.push((function() {
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.attributeBuffers[k]);
-      gl.enableVertexAttribArray(shaderLocations[k]);
-      return gl.vertexAttribPointer(shaderLocations[k], this.attributeInfos[k].elements, this.attributeInfos[k].glType, false, 0, 0);
-    }).call(this));
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.attributeBuffers[k]);
+    gl.enableVertexAttribArray(shaderLocations[k]);
+    gl.vertexAttribPointer(shaderLocations[k], this.attributeInfos[k].elements, this.attributeInfos[k].glType, false, 0, 0);
   }
-  return _result;
+  return null;
 };
 CircularAttributeBuffers.prototype.destroy = function() {
   var _i, _len, _ref, buffer;
@@ -87,5 +77,6 @@ CircularAttributeBuffers.prototype.destroy = function() {
   }
   this.attributeBuffers = [];
   this.attributeQueues = [[]];
-  return (this.attributeInfos = []);
+  this.attributeInfos = [];
+  return null;
 };
