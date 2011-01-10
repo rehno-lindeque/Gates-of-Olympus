18e18abe9407083e048633445f1660ad991d8249
Theunis <theunis@theunis-desktop.(none)>
2011-01-11 01:01:15 +0200

    Added basic spawning - 20 waves, cycles of Scorpion, fish and snakes

--- events.coffee
+++ events.coffee
@@ -7,12 +7,40 @@ This game is licensed under GPL Version 2. See http://gatesofolympus.com/LICENSE
 Setup game events
 ###
 
+numCreeps = 5
+wave = 0
+
 timeline = new Timeline
 
-spawnScorpions = (time) ->
-  #alert "Spawn scorpions"
+spawnScorpion = (time) ->
   level.creatures.addCreature(Scorpion)
-  timeline.addEvent(time + 1.0, spawnScorpions)
-  null
+  
+spawnFish = (time) ->
+  level.creatures.addCreature(Fish)
+  
+spawnSnake = (time) ->
+  level.creatures.addCreature(Snake)
 
-timeline.addEvent(1.0, spawnScorpions)
+# very hacky but for now as to allow the events to fire in time, just add the new waves here (will fix after comp)
+spawnCreepsEvent = (time) ->
+  creepType = spawnScorpion
+  if (wave % 3 == 1)
+    creepType = spawnFish
+  else if (wave % 3 == 2)
+    creepType = spawnSnake
+  
+  spawnCreeps(time, numCreeps, creepType)
+  wave++
+  if (wave % 2 == 0)
+    numCreeps++
+  timeline.addEvent(1.0+ 15.0*wave, spawnCreepsEvent)
+  
+spawnCreeps = (time, num, type) ->
+  #alert "Spawn scorpions"
+  for i in [0..num]
+    timeline.addEvent(time + i*0.5, type)
+  null
+  
+  
+timeline.addEvent(1.0, spawnCreepsEvent)
+
--- js/events.js
+++ js/events.js
@@ -1,4 +1,4 @@
-var spawnScorpions, timeline;
+var numCreeps, spawnCreeps, spawnCreepsEvent, spawnFish, spawnScorpion, spawnSnake, timeline, wave;
 /*
 Copyright 2010-2011, Rehno Lindeque, Theunis Kotze.
 This game is licensed under GPL Version 2. See http://gatesofolympus.com/LICENSE for more information.
@@ -6,10 +6,38 @@ This game is licensed under GPL Version 2. See http://gatesofolympus.com/LICENSE
 /*
 Setup game events
 */
+numCreeps = 5;
+wave = 0;
 timeline = new Timeline();
-spawnScorpions = function(time) {
-  level.creatures.addCreature(Scorpion);
-  timeline.addEvent(time + 1.0, spawnScorpions);
+spawnScorpion = function(time) {
+  return level.creatures.addCreature(Scorpion);
+};
+spawnFish = function(time) {
+  return level.creatures.addCreature(Fish);
+};
+spawnSnake = function(time) {
+  return level.creatures.addCreature(Snake);
+};
+spawnCreepsEvent = function(time) {
+  var creepType;
+  creepType = spawnScorpion;
+  if (wave % 3 === 1) {
+    creepType = spawnFish;
+  } else if (wave % 3 === 2) {
+    creepType = spawnSnake;
+  }
+  spawnCreeps(time, numCreeps, creepType);
+  wave++;
+  if (wave % 2 === 0) {
+    numCreeps++;
+  }
+  return timeline.addEvent(1.0 + 15.0 * wave, spawnCreepsEvent);
+};
+spawnCreeps = function(time, num, type) {
+  var i;
+  for (i = 0; (0 <= num ? i <= num : i >= num); (0 <= num ? i += 1 : i -= 1)) {
+    timeline.addEvent(time + i * 0.25, type);
+  }
   return null;
 };
-timeline.addEvent(1.0, spawnScorpions);
+timeline.addEvent(1.0, spawnCreepsEvent);
